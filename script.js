e4af5f970a0caf22efba16a94f40a3c3b2ac068a
larebsyed <lareb_khalil_syed@hotmail.com>
2021-07-01 08:29:26 +0500

    『❌』Delete: files

--- script.js
+++ script.js
@@ -1,128 +1,151 @@
-var margin = 10,
-    outerDiameter = 960,
-    innerDiameter = outerDiameter - margin - margin;
+var diameter = 960,
+    radius = diameter / 2,
+    innerRadius = radius - 120;
 
-var x = d3.scale.linear().range([0, innerDiameter]);
+var cluster = d3.cluster().size([360, innerRadius]);
 
-var y = d3.scale.linear().range([0, innerDiameter]);
-
-var color = d3.scale
-    .linear()
-    .domain([-1, 5])
-    .range(["hsl(185,60%,99%)", "hsl(187,40%,70%)"])
-    .interpolate(d3.interpolateHcl);
-
-var pack = d3.layout
-    .pack()
-    .padding(2)
-    .size([innerDiameter, innerDiameter])
-    .value(function (d) {
-        return d.size;
+var line = d3
+    .radialLine()
+    .curve(d3.curveBundle.beta(0.85))
+    .radius(function (d) {
+        return d.y;
+    })
+    .angle(function (d) {
+        return (d.x / 180) * Math.PI;
     });
 
 var svg = d3
     .select("body")
     .append("svg")
-    .attr("width", outerDiameter)
-    .attr("height", outerDiameter)
+    .attr("width", diameter)
+    .attr("height", diameter)
     .append("g")
-    .attr("transform", "translate(" + margin + "," + margin + ")");
+    .attr("transform", "translate(" + radius + "," + radius + ")");
 
-d3.json("hotspot_proto.json", function (error, root) {
-    var focus = root,
-        nodes = pack.nodes(root);
+var link = svg.append("g").selectAll(".link"),
+    node = svg.append("g").selectAll(".node");
 
-    svg.append("g")
-        .selectAll("circle")
-        .data(nodes)
-        .enter()
-        .append("circle")
-        .attr("class", function (d) {
-            return d.parent
-                ? d.children
-                    ? "node"
-                    : "node node--leaf"
-                : "node node--root";
-        })
-        .attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        })
-        .attr("r", function (d) {
-            return d.r;
-        })
-        .style("fill", function (d) {
-            return d.weight > 0.0
-                ? "darkred"
-                : d.children
-                ? color(d.depth)
-                : "black";
-        })
-        .style("fill-opacity", function (d) {
-            return d.weight;
-        })
-        .on("click", function (d) {
-            return zoom(focus == d ? root : d);
-        });
+d3.json("edgebundling.json", function (error, classes) {
+    if (error) throw error;
 
-    svg.append("g")
-        .selectAll("text")
-        .data(nodes)
-        .enter()
-        .append("text")
-        .attr("class", "label")
-        .attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        })
-        .style("fill-opacity", function (d) {
-            return d.parent === root ? 1 : 0;
-        })
-        .style("display", function (d) {
-            return d.parent === root ? null : "none";
-        })
-        .text(function (d) {
-            return d.name;
-        });
-
-    d3.select(window).on("click", function () {
-        zoom(root);
+    var root = packageHierarchy(classes).sum(function (d) {
+        return d.size;
     });
 
-    function zoom(d, i) {
-        var focus0 = focus;
-        focus = d;
+    cluster(root);
 
-        var k = innerDiameter / d.r / 2;
-        x.domain([d.x - d.r, d.x + d.r]);
-        y.domain([d.y - d.r, d.y + d.r]);
-        d3.event.stopPropagation();
+    link = link
+        .data(packageImports(root.leaves()))
+        .enter()
+        .append("path")
+        .each(function (d) {
+            (d.source = d[0]), (d.target = d[d.length - 1]);
+        })
+        .attr("class", "link")
+        .attr("d", line);
 
-        var transition = d3
-            .selectAll("text,circle")
-            .transition()
-            .duration(d3.event.altKey ? 7500 : 750)
-            .attr("transform", function (d) {
-                return "translate(" + x(d.x) + "," + y(d.y) + ")";
-            });
-
-        transition.filter("circle").attr("r", function (d) {
-            return k * d.r;
-        });
-
-        transition
-            .filter("text")
-            .filter(function (d) {
-                return d.parent === focus || d.parent === focus0;
-            })
-            .style("fill-opacity", function (d) {
-                return d.parent === focus ? 1 : 0;
-            })
-            .each("start", function (d) {
-                if (d.parent === focus) this.style.display = "inline";
-            })
-            .each("end", function (d) {
-                if (d.parent !== focus) this.style.display = "none";
-            });
-    }
+    node = node
+        .data(root.leaves())
+        .enter()
+        .append("text")
+        .attr("class", "node")
+        .attr("dy", "0.31em")
+        .attr("transform", function (d) {
+            return (
+                "rotate(" +
+                (d.x - 90) +
+                ")translate(" +
+                (d.y + 8) +
+                ",0)" +
+                (d.x < 180 ? "" : "rotate(180)")
+            );
+        })
+        .attr("text-anchor", function (d) {
+            return d.x < 180 ? "start" : "end";
+        })
+        .text(function (d) {
+            return d.data.key;
+        })
+        .on("mouseover", mouseovered)
+        .on("mouseout", mouseouted);
 });
 
-d3.select(self.frameElement).style("height", outerDiameter + "px");
+function mouseovered(d) {
+    node.each(function (n) {
+        n.target = n.source = false;
+    });
+
+    link.classed("link--target", function (l) {
+        if (l.target === d) return (l.source.source = true);
+    })
+        .classed("link--source", function (l) {
+            if (l.source === d) return (l.target.target = true);
+        })
+        .filter(function (l) {
+            return l.target === d || l.source === d;
+        })
+        .raise();
+
+    node.classed("node--target", function (n) {
+        return n.target;
+    }).classed("node--source", function (n) {
+        return n.source;
+    });
+}
+
+function mouseouted(d) {
+    link.classed("link--target", false).classed("link--source", false);
+
+    node.classed("node--target", false).classed("node--source", false);
+}
+
+// Lazily construct the package hierarchy from class names.
+function packageHierarchy(classes) {
+    var map = {};
+
+    function find(name, data) {
+        var node = map[name],
+            i;
+        if (!node) {
+            node = map[name] = data || { name: name, children: [] };
+            if (name.length) {
+                node.parent = find(
+                    name.substring(0, (i = name.lastIndexOf("/")))
+                );
+                node.parent.children.push(node);
+                node.key = name.substring(i + 1);
+            }
+        }
+        return node;
+    }
+
+    classes.forEach(function (d) {
+        find(d.name, d);
+    });
+
+    return d3.hierarchy(map[""]);
+}
+
+// Return a list of imports for the given array of nodes.
+function packageImports(nodes) {
+    var map = {},
+        imports = [];
+
+    // Compute a map from name to node.
+    nodes.forEach(function (d) {
+        map[d.data.name] = d;
+    });
+
+    // For each import, construct a link from the source to target node.
+    nodes.forEach(function (d) {
+        if (d.data.imports)
+            d.data.imports.forEach(function (i) {
+                if (map[i]) {
+                    // skip nodes that do not have a pair
+                    imports.push(map[d.data.name].path(map[i]));
+                }
+            });
+    });
+
+    return imports;
+}
